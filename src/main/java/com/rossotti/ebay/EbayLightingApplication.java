3bae7c8f8183d43fd3093281213cbb25bdbe64af
Paul Rossotti <rossotti.paul@gmail.com>
2022-09-16 11:40:51 -0700

    convert to spring boot project

---
+++ src/main/java/com/rossotti/ebay/EbayLightingApplication.java
@@ -0,0 +1,13 @@
+package com.rossotti.ebay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayLightingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayLightingApplication.class, args);
+	}
+
+}

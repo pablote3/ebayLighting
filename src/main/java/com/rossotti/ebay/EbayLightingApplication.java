07468985733864e4ba9f4160cc7f6f0b18ee9849
Paul Rossotti <rossotti.paul@gmail.com>
2022-09-17 19:01:01 -0700

    rename package to com.rossotti.ebay

--- src/main/java/com/rossotti/ebay/EbayLightingApplication.java
+++ src/main/java/com/rossotti/ebay/EbayLightingApplication.java
@@ -1,4 +1,4 @@
-package com.rossotti.web;
+package com.rossotti.ebay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/rossotti/ebay/EbayLightingApplicationTests.java
+++ src/test/java/com/rossotti/ebay/EbayLightingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.rossotti.web;
+package com.rossotti.ebay;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

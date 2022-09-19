077e541f8ec412e7cd36e0e6a8ecb80c3812c363
Paul Rossotti <rossotti.paul@gmail.com>
2022-09-18 20:42:57 -0700

    added marketplaceId

--- src/main/java/com/rossotti/ebay/config/WebClientProperties.java
+++ src/main/java/com/rossotti/ebay/config/WebClientProperties.java
@@ -10,4 +10,5 @@ import org.springframework.context.annotation.Configuration;
 @NoArgsConstructor
 public class WebClientProperties {
     private String baseUrl;
+    private String marketplaceId;
 }

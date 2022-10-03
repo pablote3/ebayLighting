6ab14f17fd57d317d8a0a4e94ebeb47604fad21c
Paul Rossotti <rossotti.paul@gmail.com>
2022-10-03 05:21:29 -0700

    added OptedInPrograms model

---
+++ src/main/java/com/rossotti/ebay/model/program/Program.java
@@ -0,0 +1,7 @@
+package com.rossotti.ebay.model.program;
+
+import lombok.Getter;
+@Getter
+public class Program {
+    private String programType;
+}
---
+++ src/main/java/com/rossotti/ebay/model/program/Programs.java
@@ -0,0 +1,9 @@
+package com.rossotti.ebay.model.program;
+
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+public class Programs {
+    private List<Program> programs = null;
+}

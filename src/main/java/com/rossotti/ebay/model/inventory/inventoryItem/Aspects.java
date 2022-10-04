6781f155d6949b4979b35816967ade5879ce66e5
Paul Rossotti <rossotti.paul@gmail.com>
2022-10-04 05:28:23 -0700

    added JsonProperty to Aspects

--- src/main/java/com/rossotti/ebay/model/inventory/inventoryItem/Aspects.java
+++ src/main/java/com/rossotti/ebay/model/inventory/inventoryItem/Aspects.java
@@ -1,14 +1,21 @@
 package com.rossotti.ebay.model.inventory.inventoryItem;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class Aspects {
+    @JsonProperty("Brand")
     private List<String> brand = null;
+    @JsonProperty("Type")
     private List<String> type = null;
+    @JsonProperty("Recording Definition")
     private List<String> recordingDefinition = null;
+    @JsonProperty("Optical Zoom")
     private List<String> opticalZoom = null;
+    @JsonProperty("Media Format")
     private List<String> mediaFormat = null;
+    @JsonProperty("Storage Type")
     private List<String> storageType = null;
 }

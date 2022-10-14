c3869143d27727235c254215ee53ee73e491c4e0
Paul Rossotti <rossotti.paul@gmail.com>
2022-10-14 05:19:19 -0700

    added tests for Map

--- src/test/java/com/rossotti/ebay/config/AppConfigTests.java
+++ src/test/java/com/rossotti/ebay/config/AppConfigTests.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 public class AppConfigTests {
@@ -22,5 +23,14 @@ public class AppConfigTests {
     @Test
     public void getInt_NotExists() {
         assertEquals(0, appConfig.getHttpTimeOutMs());
+    }
+
+    @Test
+    public void getMap_Exists() {
+        assertEquals("sell/account/v1/payment_policy", appConfig.getResourceMap().get("payment_policy"));
+    }
+    @Test
+    public void getMap_NotExists() {
+        assertNull(appConfig.getResourceMap().get("lala"));
     }
 }

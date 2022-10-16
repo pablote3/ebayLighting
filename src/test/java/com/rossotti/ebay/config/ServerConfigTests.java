b5ab7be8673097977f57e3450873df7065006bb8
Paul Rossotti <rossotti.paul@gmail.com>
2022-10-15 21:39:13 -0700

    added ServerConfigTests

---
+++ src/test/java/com/rossotti/ebay/config/ServerConfigTests.java
@@ -0,0 +1,25 @@
+package com.rossotti.ebay.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+@ActiveProfiles(profiles = "LOCAL")
+public class ServerConfigTests {
+    @Autowired
+    ServerConfig serverConfig;
+
+    @Test
+    public void getString_Exists() {
+        assertEquals("localhost", serverConfig.getHost());
+    }
+    @Test
+    public void getString_NotExists() {
+        assertNull(serverConfig.getScheme());
+    }
+}

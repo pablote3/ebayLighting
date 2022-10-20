bdb6c63968cd70bb69eb3ec21992353131bd05bb
Paul Rossotti <rossotti.paul@gmail.com>
2022-10-19 17:23:32 -0700

    added ReturnPolicyClient plus tests

---
+++ src/main/java/com/rossotti/ebay/client/ReturnPolicyClient.java
@@ -0,0 +1,53 @@
+package com.rossotti.ebay.client;
+
+import com.rossotti.ebay.config.AppConfig;
+import com.rossotti.ebay.config.ServerConfig;
+import com.rossotti.ebay.config.WebClientProperties;
+import com.rossotti.ebay.model.account.returnPolicy.ReturnPolicies;
+import com.rossotti.ebay.model.account.returnPolicy.ReturnPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+@Component
+public class ReturnPolicyClient extends BaseClient {
+    private final WebClientProperties properties;
+    private static final Logger logger = LoggerFactory.getLogger(ReturnPolicyClient.class);
+
+    public ReturnPolicyClient(WebClient webClient, WebClientProperties properties, AppConfig appConfig, ServerConfig serverConfig) {
+        this.webClient = webClient;
+        this.properties = properties;
+        this.appConfig = appConfig;
+        this.serverConfig = serverConfig;
+    }
+
+    public Optional<ReturnPolicy> getByReturnPolicyId(final String returnPolicyId) {
+        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
+        if (isNotBlank(returnPolicyId)) {
+            builder.path("/" + returnPolicyId);
+        }
+        builder.queryParam("marketplace_id", properties.getMarketplaceId());
+        properties.setUri(builder.build().toUri());
+        properties.setMethod(HttpMethod.GET);
+        logger.info(builder.build().toUriString());
+        properties.setHeaders(createHeaders(properties));
+        return webClientCall(properties, ReturnPolicy.class);
+    }
+
+    public Optional<ReturnPolicies> getReturnPolicies() {
+        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
+        builder.queryParam("marketplace_id", properties.getMarketplaceId());
+        properties.setUri(builder.build().toUri());
+        properties.setMethod(HttpMethod.GET);
+        logger.info(builder.build().toUriString());
+        properties.setHeaders(createHeaders(properties));
+        return webClientCall(properties, ReturnPolicies.class);
+    }
+}
---
+++ src/test/java/com/rossotti/ebay/client/ReturnPolicyClientTests.java
@@ -0,0 +1,126 @@
+package com.rossotti.ebay.client;
+
+import com.rossotti.ebay.config.AppConfig;
+import com.rossotti.ebay.config.ServerConfig;
+import com.rossotti.ebay.config.WebClientProperties;
+import com.rossotti.ebay.model.account.returnPolicy.ReturnPolicies;
+import com.rossotti.ebay.model.account.returnPolicy.ReturnPolicy;
+import com.rossotti.ebay.util.TestUtil;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ReturnPolicyClientTests {
+    private static final String RETURN_POLICY_JSON = "data/account/returnPolicy.json";
+    private static final String RETURN_POLICIES_JSON = "data/account/returnPolicies.json";
+    private static final String pathKey = "return_policy";
+    private static MockWebServer mockWebServer;
+    @Autowired
+    private AppConfig appConfig;
+    @Autowired
+    private ReturnPolicyClient returnPolicyClient;
+
+    @BeforeEach
+    public void setup() {
+        mockWebServer = new MockWebServer();
+        WebClientProperties properties = TestUtil.createWebClientProperties(mockWebServer.url("/"), appConfig, pathKey);
+        ServerConfig serverConfig = TestUtil.createServerConfig(mockWebServer.url("/"));
+        returnPolicyClient = new ReturnPolicyClient(WebClient.create(), properties, appConfig, serverConfig);
+    }
+
+    @AfterAll
+    public static void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    void returnPolicy_requestSerialization() throws InterruptedException {
+        String str = TestUtil.readStringFromFile(RETURN_POLICY_JSON).orElse(null);
+        assertNotNull(str);
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(str)
+        );
+        returnPolicyClient.getByReturnPolicyId("6196944000");
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("GET", request.getMethod());
+        assertEquals("/sell/account/v1/return_policy/6196944000?marketplace_id=EBAY_US", request.getPath());
+    }
+
+    @Test
+    void returnPolicy_responseDeserialization() {
+        String json = TestUtil.readStringFromFile(RETURN_POLICY_JSON).orElse(null);
+        Assertions.assertNotNull(json);
+        mockWebServer.enqueue(
+            new MockResponse()
+                .setResponseCode(200)
+                .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .setBody(json)
+        );
+        Optional<ReturnPolicy> response = returnPolicyClient.getByReturnPolicyId("6196944000");
+
+        assertTrue(response.isPresent());
+        assertEquals("eBay Returns EBAY_US", response.get().getName());
+        assertEquals("ALL_EXCLUDING_MOTORS_VEHICLES", response.get().getCategoryTypes().get(0).getName());
+        assertTrue(response.get().getCategoryTypes().get(0).getDefaultValue());
+        assertEquals("eBay Returns EBAY_US", response.get().getName());
+        assertEquals("ALL_EXCLUDING_MOTORS_VEHICLES", response.get().getCategoryTypes().get(0).getName());
+        assertTrue(response.get().getCategoryTypes().get(0).getDefaultValue());
+        assertEquals("DAY", response.get().getReturnPeriod().getUnit());
+    }
+
+    @Test
+    void returnPolicies_requestSerialization() throws InterruptedException {
+        String str = TestUtil.readStringFromFile(RETURN_POLICIES_JSON).orElse(null);
+        assertNotNull(str);
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(str)
+        );
+        returnPolicyClient.getReturnPolicies();
+        RecordedRequest request = mockWebServer.takeRequest();
+
+        assertEquals("GET", request.getMethod());
+        assertEquals("/sell/account/v1/return_policy?marketplace_id=EBAY_US", request.getPath());
+    }
+
+    @Test
+    void returnPolicies_responseDeserialization() {
+        String json = TestUtil.readStringFromFile(RETURN_POLICIES_JSON).orElse(null);
+        assertNotNull(json);
+        mockWebServer.enqueue(
+                new MockResponse()
+                        .setResponseCode(200)
+                        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .setBody(json)
+        );
+        Optional<ReturnPolicies> response = returnPolicyClient.getReturnPolicies();
+
+        assertTrue(response.isPresent());
+        Assertions.assertEquals(1, response.get().getTotal());
+        Assertions.assertEquals("eBay Returns EBAY_US", response.get().getReturnPolicies().get(0).getName());
+        Assertions.assertEquals("ALL_EXCLUDING_MOTORS_VEHICLES", response.get().getReturnPolicies().get(0).getCategoryTypes().get(0).getName());
+        Assertions.assertTrue(response.get().getReturnPolicies().get(0).getCategoryTypes().get(0).getDefaultValue());
+        Assertions.assertEquals("DAY", response.get().getReturnPolicies().get(0).getReturnPeriod().getUnit());
+    }
+}

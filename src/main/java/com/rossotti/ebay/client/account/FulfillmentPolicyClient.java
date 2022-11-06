403bff2232638448823bbc06d532b651dcfe5bfb
Paul Rossotti <rossotti.paul@gmail.com>
2022-11-06 15:23:08 -0800

    convert FulfillmentPolicyClient to BaseClient

--- src/main/java/com/rossotti/ebay/client/account/FulfillmentPolicyClient.java
+++ src/main/java/com/rossotti/ebay/client/account/FulfillmentPolicyClient.java
@@ -1,27 +1,25 @@
 package com.rossotti.ebay.client.account;
 
 import com.rossotti.ebay.client.BaseClient;
+import com.rossotti.ebay.client.util.QueryParam;
+import com.rossotti.ebay.client.util.QueryParamEnum;
 import com.rossotti.ebay.config.AppConfig;
 import com.rossotti.ebay.config.ServerConfig;
 import com.rossotti.ebay.client.util.WebClientProperties;
 import com.rossotti.ebay.model.account.fulfillmentPolicy.FulfillmentPolicies;
 import com.rossotti.ebay.model.account.fulfillmentPolicy.FulfillmentPolicy;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Component
 public class FulfillmentPolicyClient extends BaseClient {
-    private WebClientProperties properties;
     private static final String pathKey = "fulfillment_policy";
-    private static final Logger logger = LoggerFactory.getLogger(FulfillmentPolicyClient.class);
 
     public FulfillmentPolicyClient(WebClient webClient, AppConfig appConfig, ServerConfig serverConfig) {
         this.webClient = webClient;
@@ -30,60 +28,31 @@ public class FulfillmentPolicyClient extends BaseClient {
     }
 
     public Optional<FulfillmentPolicy> getByFulfillmentPolicyId(final String fulfillmentPolicyId) {
-        properties = createWebClientProperties(pathKey);
-        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
-        if (isNotBlank(fulfillmentPolicyId)) {
-            builder.path("/" + fulfillmentPolicyId);
-        }
-        builder.queryParam("marketplace_id", properties.getMarketplaceId());
-        properties.setUri(builder.build().toUri());
-        properties.setMethod(HttpMethod.GET);
-        logger.info(builder.build().toUriString());
-        properties.setHeaders(createHeaders());
+        List<QueryParam> queryParams = new ArrayList<>();
+        queryParams.add(new QueryParam(QueryParamEnum.MARKETPLACE_ID, appConfig.getMarketplaceId().getCode()));
+        WebClientProperties properties = buildProperties(pathKey, HttpMethod.GET, fulfillmentPolicyId, queryParams);
+        properties.getHeaders().add(HttpHeaders.CONTENT_LANGUAGE, appConfig.getContentLanguage());
         return webClientCall(properties, FulfillmentPolicy.class);
     }
     public Optional<FulfillmentPolicies> getFulfillmentPolicies() {
-        properties = createWebClientProperties(pathKey);
-        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
-        builder.queryParam("marketplace_id", properties.getMarketplaceId());
-        properties.setUri(builder.build().toUri());
-        properties.setMethod(HttpMethod.GET);
-        logger.info(builder.build().toUriString());
-        properties.setHeaders(createHeaders());
+        List<QueryParam> queryParams = new ArrayList<>();
+        queryParams.add(new QueryParam(QueryParamEnum.MARKETPLACE_ID, appConfig.getMarketplaceId().getCode()));
+        WebClientProperties properties = buildProperties(pathKey, HttpMethod.GET, null, queryParams);
         return webClientCall(properties, FulfillmentPolicies.class);
     }
     public Optional<FulfillmentPolicy> create(final FulfillmentPolicy fulfillmentPolicy) {
-        properties = createWebClientProperties(pathKey);
-        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
-        builder.queryParam("marketplace_id", properties.getMarketplaceId());
-        properties.setUri(builder.build().toUri());
-        properties.setMethod(HttpMethod.POST);
-        logger.info(builder.build().toUriString());
-        properties.setHeaders(createHeaders());
+        List<QueryParam> queryParams = new ArrayList<>();
+        queryParams.add(new QueryParam(QueryParamEnum.MARKETPLACE_ID, appConfig.getMarketplaceId().getCode()));
+        WebClientProperties properties = buildProperties(pathKey, HttpMethod.POST, null, queryParams);
+        properties.getHeaders().add(HttpHeaders.CONTENT_LANGUAGE, appConfig.getContentLanguage());
         return webClientCall(properties, FulfillmentPolicy.class, fulfillmentPolicy);
     }
     public Optional<FulfillmentPolicy> update(final FulfillmentPolicy fulfillmentPolicy, final String fulfillmentPolicyId) {
-        properties = createWebClientProperties(pathKey);
-        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
-        if (isNotBlank(fulfillmentPolicyId)) {
-            builder.path("/" + fulfillmentPolicyId);
-        }
-        properties.setUri(builder.build().toUri());
-        properties.setMethod(HttpMethod.PUT);
-        logger.info(builder.build().toUriString());
-        properties.setHeaders(createHeaders());
+        WebClientProperties properties = buildProperties(pathKey, HttpMethod.PUT, fulfillmentPolicyId, null);
         return webClientCall(properties, FulfillmentPolicy.class, fulfillmentPolicy);
     }
     public Optional<FulfillmentPolicy> delete(final String fulfillmentPolicyId) {
-        properties = createWebClientProperties(pathKey);
-        UriComponentsBuilder builder = baseUriComponentBuilder(properties);
-        if (isNotBlank(fulfillmentPolicyId)) {
-            builder.path("/" + fulfillmentPolicyId);
-        }
-        properties.setUri(builder.build().toUri());
-        properties.setMethod(HttpMethod.DELETE);
-        logger.info(builder.build().toUriString());
-        properties.setHeaders(createHeaders());
+        WebClientProperties properties = buildProperties(pathKey, HttpMethod.DELETE, fulfillmentPolicyId, null);
         return webClientCall(properties, FulfillmentPolicy.class);
     }
 }

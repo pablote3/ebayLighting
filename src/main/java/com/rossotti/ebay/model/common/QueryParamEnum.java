62c443f9d9f7d4d18164cad3e713b1fd00d1c794
Paul Rossotti <rossotti.paul@gmail.com>
2022-11-05 20:04:06 -0700

    added QueryParamEnum

---
+++ src/main/java/com/rossotti/ebay/model/common/QueryParamEnum.java
@@ -0,0 +1,15 @@
+package com.rossotti.ebay.model.common;
+
+public enum QueryParamEnum {
+    LIMIT ("limit"),
+    OFFSET ("offset"),
+    MARKETPLACE_ID ("marketplace_id"),
+    SKU ("sku");
+    private String code;
+    QueryParamEnum(String code) {
+        this.code = code;
+    }
+    public String getCode() {
+        return code;
+    }
+}
